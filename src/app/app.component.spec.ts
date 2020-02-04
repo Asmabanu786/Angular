11b3d04fa2387e109a0b988377234d8dd621bd7f
Asmabanu786 <59757764+Asmabanu786@users.noreply.github.com>
2020-02-04 06:02:32 -0500

    Add files via upload
    Angular program 2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'proj1'`, () => {
+  it(`should have as title 'proj2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('proj1');
+    expect(app.title).toEqual('proj2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('proj1 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('proj2 app is running!');
   });
 });
